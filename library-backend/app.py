6fd21eba035bef791ebd1c09f6a4ce0cb2377385
hcrkgn <hacerakgun003@gmail.com>
2026-07-13 18:20:04 +0300

    Add Flask API route

--- library-backend/app.py
+++ library-backend/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+books = [
+    {
+        "id":1,
+        "title":"Harry Potter",
+        "author": "J.K. Rowling"
+    },
+    {
+        "id":2,
+        "title": "Atomic Habits",
+        "author":"James Clear"
+    },
+    {
+        "id":3,
+        "title":"Clean Code",
+        "author":"Robert C. Martin"
+    },
+]
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "hello world"
+
+@app.route("/api/books")
+def get_books():
+    return jsonify(books)
 
 if __name__ == "__main__":
     app.run(debug=True)
